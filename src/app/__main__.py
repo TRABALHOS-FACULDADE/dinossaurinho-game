cf891345ba3999de210cc5b477dbdc2cc5972275
Felipe Ribeiro <feliper.dev@gmail.com>
2024-09-06 00:31:03 -0300

    feat: Teste inicial do game no template

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,16 +1,46 @@
-"""
-This is the main entry point of the application.
+from chrome_trex import DinoGame, ACTION_UP, ACTION_FORWARD, ACTION_DOWN
 
-Import the bare minimum needed and add any initialization code here.
-"""
+
+FPS = 60
+
+def initGame():
+
+    game = DinoGame(FPS)
+
+    action = ACTION_FORWARD
+
+    score = 0
+
+    dead_count = 0
+
+    state = []
+
+    while True:
+
+        dino = game.player_dinos[0]
+        state = game.get_state()
+        score = game.get_score()
+
+        game.step(action)
+
+        if dino.is_dead:
+            dead_count += 1
+            if dead_count == 2:
+                game.close()
+                break
+            
+            game.reset()
+    
+    return score, state
+
 
 
 def main():
-    """
-    Main entry point of the application. This is where the application starts.
-    """
-    print("Hello, World!")
-    return 0
+    print("Joguinho funfando! YAY")
+    score, state = initGame()
+    print(f'Final score: {score}')
+    print(f'States: {state}')
+
 
 
 if __name__ == "__main__":
