aed92cab7905381a8332e9e81beb3c5d3b8dd0ee
Felipe Ribeiro <feliper.dev@gmail.com>
2024-09-19 19:45:48 -0300

    alteracoes

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,59 +1,68 @@
 import random as rnd
 import numpy as np
-from chrome_trex import DinoGame, ACTION_UP, ACTION_FORWARD, ACTION_DOWN
+from chrome_trex import MultiDinoGame, Dino, ACTION_UP, ACTION_FORWARD, ACTION_DOWN
 
 # Configurações do algoritmo genético
 POPULATION_SIZE = 50
-GENERATIONS = 100
-MUTATION_RATE = 0.1
+GENERATIONS = 15
+MUTATION_RATE = 0.3
 FPS = 60
-BIAS = 0
+BIAS = 10
 
-game = DinoGame(FPS)
+game = MultiDinoGame(POPULATION_SIZE, FPS)
+
+class Dinossauro:
+    def __init__(self, gameDino: Dino) -> None:
+        self.gameDino = gameDino
+        self.pesos = [rnd.randint(-1000, 1000) for _ in range(11)]
+    
+    def tomarDecisao(self, state: list) -> int:
+        avg = np.average(state, weights=self.pesos)
+        
+        return ACTION_UP if avg > 0 else ACTION_DOWN
+    
+    def mutate(self):
+        for i in range(len(self.pesos)):
+            prob = rnd.random()
+            if prob <= MUTATION_RATE:
+                self.pesos[i] = rnd.randint(-1000, 1000)
+
 
 def softmax(x: list) -> np.ndarray:
     exp_x = np.exp(x)
     return exp_x / np.sum(exp_x)
 
-class Dinossauro:
-    def __init__(self) -> None:
-        self.score = 0
-        self.pesoForward = rnd.randint(0, 100) / 10
-        self.pesoUp = rnd.randint(0, 100) / 10
-        self.pesoDown = rnd.randint(0, 100) / 10
-    
+def selecao(dinos: list[Dinossauro]):
+    return max(dinos, key=lambda d: d.gameDino.score)
 
-    def tomarDecisao(self, state: list) -> int:
-        x_obs, y_obs = state[0], state[1]
-
-        acao_up: float = x_obs * y_obs * self.pesoUp + BIAS
-        acao_down: float = x_obs * y_obs * self.pesoDown + BIAS
-        acao_forward: float = x_obs * y_obs * self.pesoForward + BIAS
-
-        result = softmax([acao_forward, acao_up, acao_down]).tolist()
-        
-        return result.index(max(result))
-
-def generate_dinos(population):
-    return [Dinossauro() for _ in range(population)]
+def generate_dinos(population, gameDinos: list[Dino]):
+    return [Dinossauro(gameDinos[i]) for i in range(population)]
 
 def run():
+    dinos = generate_dinos(POPULATION_SIZE, game.alive_players)
+    
+    for _ in range(GENERATIONS):
+    
+        while len(game.alive_players) > 0:
+            actions = [dino.tomarDecisao(state) for dino, state in zip(dinos, game.get_state())]
+            game.step(actions)
 
-    dinos = generate_dinos(POPULATION_SIZE)
+        melhor_dino = selecao(dinos)
 
-    i = 0
+        # Repovoando jogo
 
-    while True:
-        dino = game.player_dinos[0]
-        player = dinos[i]
-        acao = player.tomarDecisao(game.get_state())
-        print(f'Geração 1 - Peso: F {player.pesoForward} U {player.pesoUp} D {player.pesoDown}')
-        game.step(acao)
-        if dino.is_dead:
-            i += 1
-            if i > POPULATION_SIZE:
-                break
-            game.reset()
+        novos_dinos = [melhor_dino]
+
+        for _ in range(POPULATION_SIZE-1):
+            melhor_dino.mutate()
+            novos_dinos.append(melhor_dino)
+
+
+        game.reset()
+
+        for k in range(len(game.alive_players)):
+            novos_dinos[k].gameDino = game.alive_players[k]
+
 
 if __name__ == '__main__':
     run()
